18289345f83d4a114c556534c52a7bd3634b436d
ndjndj <teq.ndj@gmail.com>
2021-01-18 05:26:09 +0900

    implement header.js

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header>
+                <h1>17-6-dishes</h1>
+            </header>
+
+        );
+    }
+}
+
+export default Header;
