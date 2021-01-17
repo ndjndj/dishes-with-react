2f04f464d43c12c373c94aabbe21d7f19cd7a702
ndjndj <teq.ndj@gmail.com>
2021-01-18 04:36:08 +0900

    implemnet button.js

--- src/elements/button.js
+++ src/elements/button.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 class Button extends React.Component {
     render() {
+        const btnDesc = this.props.desc;
         return (
             <div>
-                +
+                {btnDesc}
             </div>
         );
     }
