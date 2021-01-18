155871af6d40d615630f00898fc9f7aac21383e1
ndjndj <teq.ndj@gmail.com>
2021-01-19 06:10:40 +0900

    fix recipe-material-atom.js

--- src/components/recipe-material-atom.js
+++ src/components/recipe-material-atom.js
@@ -4,7 +4,7 @@ class RecipeMaterialAtom extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <div className="material-title">{this.props.name}</div>
+                <div className="material-title">{"Material" + this.props.num}</div>
                 <div className="material-amount"><input type="number"/></div>
                 <div className="material-unit"><input type="text" /></div>
             </React.Fragment>
