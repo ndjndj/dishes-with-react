44f5ca44606f8da64276486251f1efda2a9fc606
ndjndj <teq.ndj@gmail.com>
2021-01-19 06:38:13 +0900

    fix recipe.js

--- src/screen/recipe.js
+++ src/screen/recipe.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Head from '../components/head';
 import Body from '../components/body';
 import Menu from '../components/menu';
-import Button from '../components/button';
 import Header from '../components/header';
 import RecipeMaterial from '../components/recipe-material';
 import RecipeTable from '../components/recipe-table';
