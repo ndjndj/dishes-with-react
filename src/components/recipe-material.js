69a2065b7a0191264b31617aa61c5f289587ca24
ndjndj <teq.ndj@gmail.com>
2021-01-19 06:11:08 +0900

    fix recipe-material.js

--- src/components/recipe-material.js
+++ src/components/recipe-material.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import Button from '../components/button';
+import RecipeMaterialAtom from '../components/recipe-material-atom';
 class RecipeMaterial extends React.Component {
     render() {
         return(
             <div className="materials">
                 <h2>Materials</h2>
                 <div className="material">
-                    <div className="material-title">Material 1</div>
-                    <div className="material-amount"><input type="number"/></div>
-                    <div className="material-unit"><input type="text" /></div>
+                    <RecipeMaterialAtom num="1" />
                 </div>
                 <Button desc="+" />
             </div>
