61dad5992e9056227ab6370255f6c3f9c12bf5c4
ndjndj <teq.ndj@gmail.com>
2021-01-19 05:39:56 +0900

    add import recipe.js

--- src/screen/recipe.js
+++ src/screen/recipe.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import Head from '../components/head';
+import Body from '../components/body';
+import Menu from '../components/menu';
+import Button from '../components/button';
+import Header from '../components/header';
+import RecipeMaterial from '../components/recipe-material';
+import RecipeStep from '../components/recipe-step';
 
 class Recipe extends React.Component {
     render() {
