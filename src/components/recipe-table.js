3b8c1323369698dda0a237f1598f5cc20d9b3a9d
ndjndj <teq.ndj@gmail.com>
2021-01-19 06:30:27 +0900

    add button recipe-table.js

--- src/components/recipe-table.js
+++ src/components/recipe-table.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import RecipeStep from '../components/recipe-step'
-
+import Button from 'button';
 class RecipeTable extends React.Component {
     render() {
         return (
-            <div className="recipe-step-table">
-                <RecipeStep title="step1"/>
-            </div>
+            <React.Fragment>
+                <div className="recipe-step-table">
+                    <RecipeStep title="step1"/>
+                    <RecipeStep title="step2"/>
+                    <RecipeStep title="step3"/>
+                    <RecipeStep title="step4"/>
+                    <RecipeStep title="step5"/>
+                </div>
+                <Button desc="+"/>
+            </React.Fragment>
+
         );
     }
 }
