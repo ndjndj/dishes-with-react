d94d945ed6d843d8afcc19c77b808fcdd1a4d6ec
ndjndj <teq.ndj@gmail.com>
2021-01-19 06:09:14 +0900

    implement recipe-material-atom.js

--- src/components/recipe-material-atom.js
+++ src/components/recipe-material-atom.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 class RecipeMaterialAtom extends React.Component {
     render() {
-        return ();
+        return (
+            <React.Fragment>
+                <div className="material-title">{this.props.name}</div>
+                <div className="material-amount"><input type="number"/></div>
+                <div className="material-unit"><input type="text" /></div>
+            </React.Fragment>
+
+        );
     }
 }
 
