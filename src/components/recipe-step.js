53c04a0bbb729eea108b3e581f6c04112b0dfd05
ndjndj <teq.ndj@gmail.com>
2021-01-19 05:51:26 +0900

    implement recipe-step.js

--- src/components/recipe-step.js
+++ src/components/recipe-step.js
@@ -3,7 +3,16 @@ import React from 'react';
 class RecipeStep extends React.Component {
     render() {
         return (
-
+            <div className="step">
+                <h2>Step1</h2>
+                <div className="step-desc">
+                    <p>
+                        samplesamplesamplesamplesamplesamplesamplesample
+                        samplesamplesamplesamplesamplesamplesamplesample
+                        samplesamplesamplesamplesamplesamplesamplesample
+                    </p>
+                </div>
+            </div>
         );
     }
 }
