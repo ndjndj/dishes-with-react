9609f4e16da90c74d601f90f51fb85375cf0d3dd
ndjndj <teq.ndj@gmail.com>
2021-01-19 05:46:55 +0900

    implement recipe-material.js

--- src/components/recipe-material.js
+++ src/components/recipe-material.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 class RecipeMaterial extends React.Component {
     render() {
-        return();
+        return(
+            <div className="materials">
+                <h2>Materials</h2>
+                <div className="material">
+                    <div className="material-title">Material 1</div>
+                    <div className="material-amount"><input type="number"/></div>
+                    <div className="material-unit"><input type="text" /></div>
+                </div>
+                <div className="btn">+</div>
+            </div>
+        );
     }
 }
 
