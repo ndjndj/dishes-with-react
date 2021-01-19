b478bd27fef38fbba8bfc1b74260012be2ffff01
ndjndj <teq.ndj@gmail.com>
2021-01-20 07:34:14 +0900

    skelton recipe-list.js

---
+++ src/components/recipe-list.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Head from '../components/head';
+import Body from '../components/body';
+import Menu from '../components/menu';
+import Header from '../components/header';
+
+class RecipeList extends React.Component {
+    render() {
+        return (
+        );
+    }
+}
+
+export default RecipeList;
