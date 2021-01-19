71a36177d99193055a612320de95f09c7f514beb
ndjndj <teq.ndj@gmail.com>
2021-01-20 07:36:13 +0900

    implement recipe-list.js

--- src/components/recipe-list.js
+++ src/components/recipe-list.js
@@ -3,10 +3,20 @@ import Head from '../components/head';
 import Body from '../components/body';
 import Menu from '../components/menu';
 import Header from '../components/header';
-
+import RecipeCardTable from '../components/recipe-card-table';
 class RecipeList extends React.Component {
     render() {
         return (
+            <React.Fragment>
+                <Head title="recipe-list" />
+                <Body>
+                    <Header />
+                    <main>
+                        <RecipeCardTable />
+                    </main>
+                    <Menu />
+                </Body>
+            </React.Fragment>
         );
     }
 }
