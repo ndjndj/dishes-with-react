867a3435f34f40467784d797a6dc3ed2395667a3
ndjndj <teq.ndj@gmail.com>
2021-01-20 07:41:48 +0900

    implement recipe-card-table.js

--- src/components/recipe-card-table.js
+++ src/components/recipe-card-table.js
@@ -1,9 +1,25 @@
 import React from 'react';
-
+import RecipeCard from '../components/recipe-card';
+import Button from '../components/button';
 class RecipeCardTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            cards: 1
+        }
+    }
     render() {
         return (
-
+            <React.Fragment>
+                <div className="recipe-card-table">
+                    {this.state.map(
+                        (card, i) => {
+                            return <RecipeCard key={i} />
+                        })
+                    }
+                </div>
+                <Button desc="+" />
+            </React.Fragment>
         );
     }
 }
