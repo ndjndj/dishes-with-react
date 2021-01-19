7127c60d47b03b7960540fce0050731ac36c1752
ndjndj <teq.ndj@gmail.com>
2021-01-20 07:27:52 +0900

    implement recipe-card

--- src/components/recipe-card.js
+++ src/components/recipe-card.js
@@ -3,7 +3,16 @@ import React from 'react';
 class RecipeCard extends React.Component {
     render() {
         return (
-
+            <div classNmae="wrapper">
+                <div className="img">img</div>
+                <div className="desc">
+                    <div className="recipe title">recipe title</div>
+                    <div className="under">
+                        <div className="category">category</div>
+                        <div className="date">created at</div>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
