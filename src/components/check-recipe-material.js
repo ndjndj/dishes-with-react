4f93cecfcbe132d27fb0cfba4c14ff86dc7edf73
ndjndj <teq.ndj@gmail.com>
2021-01-21 04:49:44 +0900

    implement check-recipe-material.js

--- src/components/check-recipe-material.js
+++ src/components/check-recipe-material.js
@@ -3,7 +3,11 @@ import React from 'react';
 class CheckRecipeMaterial extends React.Component {
     render () {
         return (
-
+            <div className="materials">
+                <h2>Materials</h2>
+                <p>material1 10g</p>
+                <p>material2 10g</p>
+            </div>
         );
     }
 }
