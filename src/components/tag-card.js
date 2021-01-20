8fe3794607695de59d7a648fd23aedf8a208c27b
ndjndj <teq.ndj@gmail.com>
2021-01-21 05:03:15 +0900

    implement tag-card.js

--- src/components/tag-card.js
+++ src/components/tag-card.js
@@ -3,7 +3,12 @@ import React from 'react';
 class TagCard extends React.Component {
     render() {
         return (
-            
+            <div className="tag-card">
+                <div className="thumb">img</div>
+                <div className="name">name</div>
+                <div className="tag">tag name</div>
+                <div className="desc-box"><p>description</p></div>
+            </div>
         );
     }
 }
