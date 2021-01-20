4f46efc1fa3e1e89f4738e9b82b3f9f34b192790
ndjndj <teq.ndj@gmail.com>
2021-01-21 04:51:41 +0900

    implement check-recipe-step.js

--- src/components/check-recipe-step.js
+++ src/components/check-recipe-step.js
@@ -3,7 +3,29 @@ import React from 'react';
 class CheckRecipeStep extends React.Component {
     render () {
         return (
+            <div className="steps">
+                <div className="step">
+                    <h2>step 1</h2>
+                    <div className="step-desc">
+                        <p>
+                            samplesamplesamplesamplesamplesamplesamplesample
+                            samplesamplesamplesamplesamplesamplesamplesample
+                            samplesamplesamplesamplesamplesamplesamplesample
+                        </p>
+                    </div>
+                </div>
 
+                <div className="step">
+                    <h2>step 2</h2>
+                    <div className="step-desc">
+                        <p>
+                            samplesamplesamplesamplesamplesamplesamplesample
+                            samplesamplesamplesamplesamplesamplesamplesample
+                            samplesamplesamplesamplesamplesamplesamplesample
+                        </p>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
