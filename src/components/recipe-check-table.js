49f92e94b5a8c3190cd1881a4b26805b7e7ea039
ndjndj <teq.ndj@gmail.com>
2021-01-21 04:44:02 +0900

    implement recipe-check-table.js

--- src/components/recipe-check-table.js
+++ src/components/recipe-check-table.js
@@ -4,7 +4,10 @@ import CheckRecipeStep from '../components/check-recipe-step';
 class RecipeCheckTable extends React.Component {
     render () {
         return (
-            
+            <React.Component>
+                <CheckRecipeMaterial />
+                <CheckRecipeStep />
+            </React.Component>
         );
     }
 }
