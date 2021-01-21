d674d2a9ecdd785593ee9e6843bb28192dc2d9bc
ndjndj <teq.ndj@gmail.com>
2021-01-22 05:04:40 +0900

    skelton App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return ();
+  }
 }
 
 export default App;
