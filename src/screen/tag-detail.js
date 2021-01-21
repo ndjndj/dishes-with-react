efb39375e788edc4a0950419c9fa8cc7b8be7b4d
ndjndj <teq.ndj@gmail.com>
2021-01-22 04:34:31 +0900

    implement tag-detail.js

--- src/screen/tag-detail.js
+++ src/screen/tag-detail.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import Head from '../components/head';
+import Body from '../components/body';
+import Menu from '../components/menu';
+import Header from '../components/header';
+import TagDetailAtom from '../components/tag-detail-atom';
 
 class TagDetail extends React.Component {
     render () {
         return(
-
+            <React.Fragment>
+                <Head />
+                <Body>
+                    <Header />
+                    <TagDetailAtom />
+                </Body>
+                <Menu />
+            </React.Fragment>
         );
     }
 }
