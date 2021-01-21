ed94ccd19c5a5b9bd99e4015237f7c76b57a090b
ndjndj <teq.ndj@gmail.com>
2021-01-22 05:07:43 +0900

    change App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import RecipeList from './screen/recipe-list';
 class App extends React.Component {
   render() {
     return (
-      
+      <RecipeList />
     );
   }
 }
