bd78e465010d4974b6cd992116a141108301ed64
ndjndj <teq.ndj@gmail.com>
2021-01-22 04:56:04 +0900

    skelton tag-card.js

--- src/components/tag-card.js
+++ src/components/tag-card.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-class TagDetaiAtom extends React.Component {
+class TagCard extends React.Component {
     render () {
         return(
         );
     }
 }
 
-export default TagDetailAtom;
+export default TagCard;
