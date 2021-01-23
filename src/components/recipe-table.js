b0d47e0330eaf7a1c466179a57bcdc2b273f80d9
ndjndj <teq.ndj@gmail.com>
2021-01-23 17:06:19 +0900

    fix

--- src/components/recipe-table.js
+++ src/components/recipe-table.js
@@ -2,21 +2,11 @@ import React from 'react';
 import RecipeStep from '../components/recipe-step'
 import Button from 'button';
 class RecipeTable extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            steps: 1
-        }
-    }
     render() {
         return (
             <React.Fragment>
                 <div className="recipe-step-table">
-                    {this.state.steps.map(
-                        (step, i) => {
-                            return <RecipeStep key={i} />
-                        })
-                    }
+                    <RecipeStep />
                 </div>
                 <Button desc="+"/>
             </React.Fragment>
