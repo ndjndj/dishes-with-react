884d1d5d22b9acf880a1d7887094b515d742ca96
ndjndj <teq.ndj@gmail.com>
2021-01-24 17:20:45 +0900

    fix button.js

--- src/components/button.js
+++ src/components/button.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import '../styles/button.css';
-import { Link } from 'react-router-dom';
+import { Router, Link } from 'react-router-dom';
 class Button extends React.Component {
     render() {
         const btnDesc = this.props.desc;
         const link = this.props.link;
+        const component = this.props.component
         return (
-            <Link to={link} className="btn">
-                {btnDesc}
-            </Link>
+            <Router>
+                <Link to={link} className="btn">
+                    {btnDesc}
+                </Link>
+                <Route exact path={link} component={component}/>
+            </Router>
         );
     }
 }
