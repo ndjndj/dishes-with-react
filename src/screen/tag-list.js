164edde71e4f620176cd6b55ca12708f6745d35c
ndjndj <teq.ndj@gmail.com>
2021-01-25 02:12:55 +0900

    fix tag-list.js

--- src/screen/tag-list.js
+++ src/screen/tag-list.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import Head from '../components/head';
-import Body from '../components/body';
 import Menu from '../components/menu';
 import Header from '../components/header';
 import TagTable from '../components/tag-table';
@@ -9,11 +7,8 @@ class TagList extends React.Component {
     render () {
         return(
             <React.Fragment>
-                <Head />
-                <Body>
-                    <Header />
-                    <TagTable />
-                </Body>
+                <Header />
+                <TagTable />
                 <Menu />
             </React.Fragment>
         );
