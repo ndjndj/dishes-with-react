92386c7f2819146ac7ffb6bc9b07fcae3fbcf66b
ndjndj <teq.ndj@gmail.com>
2021-01-24 17:11:39 +0900

    import react-router-dom

--- src/components/button.js
+++ src/components/button.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styles/button.css';
+import { Link } from 'react-router-dom';
 class Button extends React.Component {
     render() {
         const btnDesc = this.props.desc;
