d81db45a2f6c25f7e6d679a6d58531bf9ca2174a
ndjndj <teq.ndj@gmail.com>
2021-01-25 02:17:08 +0900

    import tag-list menu.js

--- src/components/menu.js
+++ src/components/menu.js
@@ -2,12 +2,13 @@ import React from 'react';
 import Button from './button';
 import '../styles/menu.css';
 import RecipeCheck from '../screen/recipe';
+import TagList from '../screen/tag-list';
 class Menu extends React.Component {
     render() {
         return (
             <div className = "menu">
                 <Button component={RecipeCheck} link="recipe" desc="recipe"  />
-                <Button component={RecipeCheck} link="tag" desc="tag" />
+                <Button component={TagList} link="tag" desc="tag" />
                 <Button component={RecipeCheck} link="add" desc="+" />
                 <Button component={RecipeCheck} link="calender" desc="calender" />
                 <Button component={RecipeCheck} link="config" desc="config" />
