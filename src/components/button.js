ef44c01e3a9586ad16bf575e647f1a4939127e15
ndjndj <teq.ndj@gmail.com>
2021-01-25 02:49:28 +0900

    fix buttton.js

--- src/components/button.js
+++ src/components/button.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import '../styles/button.css';
-import { Route, BrowserRouter as Router, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 class Button extends React.Component {
     render() {
         const btnDesc = this.props.desc;
         const link = this.props.link;
-        const component = this.props.component;
         return (
-            <Router>
-                <Link to={link} className="btn">
-                    {btnDesc}
-                </Link>
-                <Route path={link} exact component={component}/>
-            </Router>
+            <Link to={link} className="btn">
+                {btnDesc}
+            </Link>
         );
     }
 }
