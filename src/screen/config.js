ec2c2d1ed058db785af4cdea75625518530bf021
ndjndj <teq.ndj@gmail.com>
2021-01-25 02:20:58 +0900

    skelton config.js

---
+++ src/screen/config.js
@@ -0,0 +1,12 @@
+import React from 'react';
+class Config extends React.Component {
+    render() {
+        return (
+            <div className = "config">
+                this component is Config.
+            </div>
+        );
+    }
+}
+
+export default Config;
