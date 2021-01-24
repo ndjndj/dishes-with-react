83ba0e75d565a88aef5a21b5dc061a51e48626b0
ndjndj <teq.ndj@gmail.com>
2021-01-25 02:47:40 +0900

    fix menu.js

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import Button from './button';
 import '../styles/menu.css';
-import RecipeCheck from '../screen/recipe';
-import TagList from '../screen/tag-list';
 class Menu extends React.Component {
     render() {
         return (
             <div className = "menu">
-                <Button component={RecipeCheck} link="recipe" desc="recipe"  />
-                <Button component={TagList} link="tag" desc="tag" />
-                <Button component={RecipeCheck} link="add" desc="+" />
-                <Button component={RecipeCheck} link="calender" desc="calender" />
-                <Button component={RecipeCheck} link="config" desc="config" />
+                <Button link="recipe" desc="recipe"  />
+                <Button link="tag" desc="tag" />
+                <Button link="add" desc="+" />
+                <Button link="calender" desc="calender" />
+                <Button link="config" desc="config" />
             </div>
         );
     }
