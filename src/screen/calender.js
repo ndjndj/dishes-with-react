b3a3a46180461f7b7e3f200c08e35b9a74c3d96c
ndjndj <teq.ndj@gmail.com>
2021-01-25 02:20:19 +0900

    skelton calender.js

---
+++ src/screen/calender.js
@@ -0,0 +1,12 @@
+import React from 'react';
+class Calender extends React.Component {
+    render() {
+        return (
+            <div className = "calender">
+                this component is Calender.
+            </div>
+        );
+    }
+}
+
+export default Calender;
