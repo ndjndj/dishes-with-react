3d6fffe9149238db176e5c4f63265d52d93726e0
ndjndj <teq.ndj@gmail.com>
2021-01-25 02:26:58 +0900

    fix button.js

--- src/components/button.js
+++ src/components/button.js
@@ -11,7 +11,7 @@ class Button extends React.Component {
                 <Link to={link} className="btn">
                     {btnDesc}
                 </Link>
-                <Route exact path={link} component={component}/>
+                <Route path={link} exact component={component}/>
             </Router>
         );
     }
