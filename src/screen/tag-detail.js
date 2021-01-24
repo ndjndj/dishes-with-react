5d39f9b4a87bc5e3c48f85bbb05d37092458eb4f
ndjndj <teq.ndj@gmail.com>
2021-01-25 02:12:20 +0900

    fix tag-detail.js

--- src/screen/tag-detail.js
+++ src/screen/tag-detail.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import Head from '../components/head';
-import Body from '../components/body';
 import Menu from '../components/menu';
 import Header from '../components/header';
 import Button from '../components/button';
@@ -10,13 +8,10 @@ class TagDetail extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <Head />
-                <Body>
-                    <Header />
-                    <Button desc="Edit" className="edit" />
-                    <TagThumb />
-                    <Menu />
-                </Body>
+                <Header />
+                <Button desc="Edit" className="edit" />
+                <TagThumb />
+                <Menu />
             </React.Fragment>
         );
     }
