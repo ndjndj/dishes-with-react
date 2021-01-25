4d96fbc39ef3d75a742f8d1cd4046f835e49482c
ndjndj <teq.ndj@gmail.com>
2021-01-26 06:51:54 +0900

    fix menu.js

--- src/components/menu.js
+++ src/components/menu.js
@@ -5,11 +5,11 @@ class Menu extends React.Component {
     render() {
         return (
             <div className = "menu">
-                <Button link="recipe" desc="recipe"  />
-                <Button link="tag" desc="tag" />
-                <Button link="add" desc="+" />
-                <Button link="calender" desc="calender" />
-                <Button link="config" desc="config" />
+                <Button link="/" desc="recipe"  />
+                <Button link="/tag" desc="tag" />
+                <Button link="/add" desc="+" />
+                <Button link="/calender" desc="calender" />
+                <Button link="/config" desc="config" />
             </div>
         );
     }
