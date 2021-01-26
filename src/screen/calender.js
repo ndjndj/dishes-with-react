34d106dae7b9e726261b2f34d6740dd5a4f27b7f
ndjndj <teq.ndj@gmail.com>
2021-01-27 00:15:39 +0900

    fix createArrDays

--- src/screen/calender.js
+++ src/screen/calender.js
@@ -5,8 +5,12 @@ class Calender extends React.Component {
         const createArrDays = (ymd) => {
             // 月の日数を取得
             let daysCount = new Date(ymd.getFullYear(), ymd.getMonth(), 0).getDate();
+            // 月の初日をDate 型で取得
+            let thisMonth = new Date(ymd.getFullYear(), ymd.getMonth() + 1, 1);
             // 1日から月末日の配列を生成
-            let arrDays = [...Array(daysCount).keys()].map((_, i) => ++i);
+            let arrDays = [...Array(daysCount).keys()].map(
+                (_, i) => new Date(new Date().setDate(thisMonth.getDate() + i))
+            );
             return arrDays;
         }
 
