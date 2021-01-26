d12764d17c6304d9612934545cd988ca9510732a
ndjndj <teq.ndj@gmail.com>
2021-01-26 22:36:52 +0900

    def createCalender with calender.js

--- src/screen/calender.js
+++ src/screen/calender.js
@@ -2,6 +2,13 @@ import React from 'react';
 import Menu from '../components/menu';
 class Calender extends React.Component {
     render() {
+        const createCalender = (ymd) => {
+            // get month
+            // create array with this month
+            return;
+        }
+
+
         return (
             <React.Fragment>
                 <div className = "calender">
