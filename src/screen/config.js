dce087c9694e050afb825692539c6b970db22ded
ndjndj <teq.ndj@gmail.com>
2021-01-29 00:45:38 +0900

    config.js

--- src/screen/config.js
+++ src/screen/config.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Menu from '../components/menu';
+import Header from '../components/header';
 class Config extends React.Component {
     render() {
         return (
             <React.Fragment>
+                <Header />
                 <div className = "config">
                     this component is Config.
                 </div>
