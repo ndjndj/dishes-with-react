898c1867900d7d60e44935d8848971f699afed4f
ndjndj <teq.ndj@gmail.com>
2021-01-29 01:28:10 +0900

    add constructor with recipe-step.js

--- src/components/recipe-table.js
+++ src/components/recipe-table.js
@@ -2,6 +2,12 @@ import React from 'react';
 import RecipeStep from '../components/recipe-step'
 import Button from '../components/button';
 class RecipeTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            stepNumber: [],
+        }
+    }
     render() {
         return (
             <React.Fragment>
