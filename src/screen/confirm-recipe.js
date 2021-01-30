701c098774b28715c51b002fa1666aea2cb2a46b
ndjndj <teq.ndj@gmail.com>
2021-01-30 23:52:27 +0900

    change confirm-recipe.js

--- src/screen/confirm-recipe.js
+++ src/screen/confirm-recipe.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Header from '../components/header';
+import Button from '../components/button';
+import RecipeCheckTable from '../components/recipe-check-table';
 import Menu from '../components/menu';
 
 class ConfirmRecipe extends React.Component {
@@ -7,6 +9,8 @@ class ConfirmRecipe extends React.Component {
         return (
             <React.Fragment>
                 <Header />
+                <Button desc="Edit" className="edit" />
+                <RecipeCheckTable />
                 <Menu />
             </React.Fragment>
         );
