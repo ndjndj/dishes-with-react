97716c5e453d87e78869be8ce391df6e9a98cb4c
ndjndj <teq.ndj@gmail.com>
2021-01-30 23:43:34 +0900

    skelton confirm-recipe.js

---
+++ src/screen/confirm-recipe.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class ConfirmRecipe extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Header />
+                <Menu />
+            </React.Fragment>
+
+        );
+    }
+}
+
+export default ConfirmRecipe;
