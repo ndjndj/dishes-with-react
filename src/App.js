a313ed346399e10db2eaf80668abe7bd32962542
ndjndj <teq.ndj@gmail.com>
2021-01-30 23:18:27 +0900

    add route App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import TagList from './screen/tag-list';
 import Recipe from './screen/recipe';
 import Calender from './screen/calender';
 import Config from './screen/config';
+import RecipeCheck from './screen/recipe-check';
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 
 class App extends React.Component {
@@ -16,6 +17,7 @@ class App extends React.Component {
             <Route path="/calender" component={Calender}/>
             <Route path="/config" component={Config}/>
             <Route path="/add" component={Recipe}/>
+            <Route path="/recipe-check" component={RecipeCheck} />
             <Route path="/" component={RecipeList}/>
           </Switch>
         </Router>
