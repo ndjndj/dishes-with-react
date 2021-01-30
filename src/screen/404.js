60eaf9fbd7ba8e55e5b3b41bc04c2f586c3d58bf
ndjndj <teq.ndj@gmail.com>
2021-01-31 01:26:59 +0900

    fix 404.js

--- src/screen/404.js
+++ src/screen/404.js
@@ -4,7 +4,7 @@ class NotFound extends React.Component {
     render () {
         return(
             <React.Fragment>
-                
+                <h1>404 Not found.</h1>
             </React.Fragment>
         );
     }
