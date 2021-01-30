f49a346772d915c68a3fcbd23153a53cf5921996
ndjndj <teq.ndj@gmail.com>
2021-01-30 23:33:05 +0900

    fix

--- src/components/recipe-check-table.js
+++ src/components/recipe-check-table.js
@@ -2,7 +2,7 @@ import React from 'react';
 import CheckRecipeMaterial from '../components/check-recipe-material';
 import CheckRecipeStep from '../components/check-recipe-step';
 class RecipeCheckTable extends React.Component {
-    render () {
+    render() {
         return (
             <React.Component>
                 <CheckRecipeMaterial />
